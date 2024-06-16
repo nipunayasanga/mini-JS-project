7e8102955c0da32c0d7db1d321b978eff10c8008
nipunayasanga <nipunayes@gmail.com>
2024-06-16 23:24:46 +0530

    update JS

--- main.js
+++ main.js
@@ -1,16 +1,40 @@
 let interval = undefined;
+let min = 0;
+let sec = 0;
 
+let minElement=document.getElementById("min");
+let secElement=document.getElementById("sec");
 
-const start = () => {
-
+const countdown = () => {
     if (interval){
         clearInterval(interval);
     }
 
-    let x=0;
 
-   interval = setInterval(() => {
-        console.log(x);
-        x++
-    }, 1000);
+}
+
+interval = setInterval(() => {
+
+    sec++;
+
+    if(sec<10){
+        secElement.innerHTML='0'+sec
+    }else{
+        secElement.innerHTML=sec
+    }
+
+
+    if(sec === 59){
+        min++;
+        sec=0;
+
+    }
+    
+}, 1000);
+
+const start = () => {
+
+    countdown();
+
+   
 }
