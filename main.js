02cd3a54f76f5afcde99610537c1bdd5aa02f876
nipunayasanga <nipunayes@gmail.com>
2024-06-24 21:46:23 +0530

    updates

--- main.js
+++ main.js
@@ -1,195 +1,136 @@
-let playerName = '';// let var const
-let level = '';  // 1l==> 0-50 => (10 * 2 = ?) 2nd 0-100 ==> (55 / 6);
-let number1=0;
-let number2=0;
-let exp='';
-operators=['+','-','/','*','%'];
-let question='';
-let time=0;
-let counter=null; // null => nothing
-let requestAnswer;
-let correctAnswer=0;
-let resultSheet=[];
-let correctAnswers=0;
-let wrongAnswers=0;
+let interval=undefined;
+let min=0;
+let sec=0;
 let questionNumber=0;
-let answerState=false;
+let count=0;
+let op=['+','-','*','/','%'];
 
-// start login form js
-letsGetStarted = () => {
-    let playerNameElement =
-        document.getElementById('playerName');
-    let levelElement =
-        document.getElementById('level');
-    setBorderColor(playerNameElement)
+let qNum1=0;
+let qNum2=0;
+let selectedOp='';
 
-    // _nimal____ trim() =>> nimal
-    let tempPlayerName = playerNameElement.value;
-    if (tempPlayerName.trim() === '') { // trim rid spaces _abc_ => after trim()==> abc
-        playerNameElement.style.borderColor = 'red';
-        alert('please insert name and level to continue!');
-        return;
-    }
+let minElement=document.getElementById('min');
+let secElement=document.getElementById('sec');
 
-    playerName = tempPlayerName;
-    level = levelElement.value;
-    //====> redirect to gaming console==>
-    let playerData={name:playerName,level:level};
-    localStorage.setItem("playerData",JSON.stringify(playerData));
-    window.location.href = "game.html";
+let countElement=document.getElementById('count');
 
-}
-setBorderColor = (element) => {
-    element.style.borderColor = '';
-}
-// end login form js
+let num1Element=document.getElementById('num1');
+let num2Element=document.getElementById('num2');
+let opElement=document.getElementById('op');
 
-// start game console js
-setPlayerData = () => {
-    let storageData = JSON.parse(localStorage.getItem("playerData"));
-    playerName = storageData.name;
-    level = storageData.level;
+let answerElement=document.getElementById('answer');
+let table=document.getElementById('result_table_body');
 
-    document.getElementById('player-name')
-        .innerHTML=playerName;
-    document.getElementById('player-level')
-        .innerHTML=level;
-}
-startGame=()=>{
-    generateQuestion();
-}
-generateQuestion=()=>{
-    questionNumber++;
-    document.getElementById('questionNumber').innerHTML=questionNumber;
+let result=[];
 
-    clearTime();
-    let selectedMax=checkLevel();
-    number1=generateNumber(1,selectedMax);
-    number2=generateNumber(1,selectedMax);
-    exp=operators[generateNumber(0,5)];
-    question = `${number1} ${exp} ${number2} =?`;
-    document.getElementById('question')
-        .innerHTML=question;
-    executeTime();
-}
-generateNumber=(min, max)=>{
-    return Math.floor(Math.random() * (max - min) + min); // specific range ==> example 1-101
-}
-checkLevel=()=>{
-    let levelNumber=51; //default Beginner
-    switch (level){
-        case "Beginner": levelNumber=51;break;
-        case "Middle": levelNumber=101;break;
-        case "Advanced": levelNumber=1001;break;
-    }
-    return levelNumber;
-}
-executeTime=()=>{
-    counter = setInterval(()=>{
-        time++;
-        document.
-        getElementsByClassName('counter-time')[0].innerHTML=time;
+const setTableData=()=>{
+    result.forEach(item=>{
+        let row = document.createElement('tr');
 
-        if(time===60){
-            clearTime();
-            alert('Failed');
+        for(let key in item){
+            if(item.hasOwnProperty(key)){
+                let cell = document.createElement('td');
+                cell.textContent=item[key];
+                row.appendChild(cell);
+            }
         }
+
+        table.appendChild(row);
+
+    });
+}
+
+
+const setAnswer=()=>{
+    let correctAnswer=0;
+    let userAnswer=parseInt(answerElement.value);
+    switch(selectedOp){
+        case '+':correctAnswer=qNum1+qNum2; break;
+        case '-':correctAnswer=qNum1-qNum2; break;
+        case '/':correctAnswer=qNum1/qNum2; break;
+        case '*':correctAnswer=qNum1*qNum2; break;
+        case '%':correctAnswer=qNum1%qNum2;
+    }
+
+    result.push({
+        'number1':qNum1,
+        'number2':qNum2,
+        'correctAnswer':correctAnswer,
+        'userAnswer':userAnswer,
+        'operator':selectedOp,
+        'time':minElement.innerHTML+':'+secElement.innerHTML,
+        'isCorrect':correctAnswer===userAnswer
+    });
+
+    answerElement.value=0;
+
+    if (count==5) {
+        count=0;
+
+        setTableData();
+
+        reset();
+    }
+
+    manageQuestion();
+
+}
+
+const setCount=()=>{
+    count++;
+    countElement.innerHTML=count;
+}
+
+const manageQuestion=()=>{
+    setCount();
+
+    qNum1= Math.floor(Math.random()*100)+1;
+    qNum2= Math.floor(Math.random()*100)+1;
+    selectedOp = op[Math.floor(Math.random()*5)];
+
+    num1Element.innerHTML=qNum1;
+    num2Element.innerHTML=qNum2;
+    opElement.innerHTML=selectedOp;
+}
+
+const reset=()=>{
+    if(interval){
+        clearInterval(interval);
+    }
+
+    minElement.innerHTML='00';
+    secElement.innerHTML='00';
+}
+
+const countdown = ()=>{
+
+    manageQuestion();
+
+    if(interval){
+        clearInterval(interval);
+    }
+
+    interval = setInterval(() => {
+    
+        sec++;
+        
+        if (sec<10) {
+            secElement.innerHTML='0'+sec
+        }else{
+            secElement.innerHTML=sec
+        }
+        
+        if(sec===59){
+            min++;
+                minElement.innerHTML=min
+            sec=0;
+        }
+
+
     }, 1000);
 }
-clearTime=()=>{
-    time=0;
-    clearInterval(counter)
+
+const start = ()=>{
+    result=[];
+    countdown();
 }
-submitAnswer=()=>{
-    requestAnswer = document.getElementById('requestAnswer').value;
-    console.log(requestAnswer);
-    if (isNaN(requestAnswer) || number1===0 || requestAnswer===''){
-        alert('please insert a number or start the game');
-        return;
-    }
-
-    findAnswer();
-    if (correctAnswer===Number(requestAnswer)){
-        // set correct and incorrect values=========
-        answerState=true;
-        correctAnswers++;
-        document.getElementById('congrats').innerHTML='Congratulations';
-        document.getElementById('congrats').style.color='#2980b9';
-        document.getElementById('correctAnswers').innerHTML=correctAnswers;
-    }else{
-        answerState=false;
-        wrongAnswers++;
-        document.getElementById('congrats').innerHTML=`Oops... (A : ${correctAnswer})`;
-        document.getElementById('congrats').style.color='#D35400';
-
-        document.getElementById('wrongAnswers').innerHTML=wrongAnswers;
-    }
-    
-
-//============================
-
-
-document.getElementById('question').innerHTML='Processing...'
-
-    setTimeout(()=>{
-           greeting();
-    }, 100);
-}
-//============================
-
-findAnswer=()=>{
-    switch (exp){
-        case "+": correctAnswer=number1+number2;break;
-        case "-": correctAnswer=number1-number2;break;
-        case "/": correctAnswer=number1/number2;break;
-        case "*": correctAnswer=number1*number2;break;
-        case "%": correctAnswer=number1%number2;
-    }
-}
-
-greeting=()=>{
-    let result={
-        question_id:questionNumber,
-        question: question,
-        request_answer: requestAnswer,
-        answer: correctAnswer,
-        state:answerState,
-        time:time
-    };
-    resultSheet.push(result);
-    //==== clear greeing
-    document.getElementById('congrats').innerHTML='';
-    document.getElementById('requestAnswer').value='';
-    if(questionNumber!==10){
-        generateQuestion();
-    }else{
-      localStorage.setItem('result',JSON.stringify(resultSheet));
-      window.location.href = "result.html";
-      // reset all data ==> you will have to do this.
-    }
-}
-
-setResult=()=>{
-    const data=JSON.parse(localStorage.getItem('result'));
-    for(const tempDataSet of data){
-        const element = document.getElementsByTagName('tbody')[0];
-        const newRow = `<tr>
-        <td>${tempDataSet.question_id}</td>
-         <td>${tempDataSet.question}</td>
-          <td>${tempDataSet.request_answer}</td>
-           <td>${tempDataSet.answer}</td>
-            <td>${tempDataSet.state?'🙂':'😢'}</td>
-             <td>${tempDataSet.time}</td>
-              </tr>`
-        element.innerHTML+=newRow;
-    }
-}
-
-// end game console js
-
-
-
-
-
-
-
