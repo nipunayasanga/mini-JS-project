ae4d816428c87dbcf5955353288e24174a40129e
nipunayasanga <nipunayes@gmail.com>
2024-06-24 19:14:38 +0530

    add random numbers & operators

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
 let interval = undefined;
 let min = 0;
 let sec = 0;
+let questionNumber = 1;
+let count = 0;
+let op = ['+','-','*','/','%'];
+
+let qNum1 = 0;
+let qNum2  = 0;
+let selectedOp = '';
 
 let minElement=document.getElementById("min");
 let secElement=document.getElementById("sec");
+
+let countElement=document.getElementById("count");
+
+let num1Element=document.getElementById("num1");
+let num2lement=document.getElementById("num2");
+let opElement=document.getElementById("op");
+
+
+const setCount = () => {
+    count++;
+    countElement.innerHTML=count;
+}
+
+const manageQuestuion = () => {
+
+    setCount();
+
+    qNum1 = Math.floor(Math.random()*100)+1;
+    qNum2 = Math.floor(Math.random()*100)+1;
+    selectedOp = op[Math.floor(Math.random()*5)]
+
+    num1Element.innerHTML = qNum1;
+    num2lement.innerHTML = qNum2;
+    opElement.innerHTML = selectedOp;
+
+  
+}
 
 const reset = () => {
     if (interval){
@@ -15,6 +49,9 @@ const reset = () => {
 }
 
 const countdown = () => {
+
+    manageQuestuion();
+
     if (interval){
         clearInterval(interval);
     }
